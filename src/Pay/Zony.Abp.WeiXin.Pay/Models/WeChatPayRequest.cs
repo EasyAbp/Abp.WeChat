66dce8f4171f13d8f360473ac2c9ebb006274d42
GameBelial <243387971@qq.com>
2019-08-23 12:09:37 +0800

    增加微信 OAuth2.0 的 Scope 定义。

---
+++ src/Pay/Zony.Abp.WeiXin.Pay/Models/WeChatPayRequest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Zony.Abp.WeiXin.Pay.Models
+{
+    /// <summary>
+    /// 微信支付参数集合，所有接口参数都必须传入。
+    /// </summary>
+    public abstract class WeChatPayRequest
+    {
+        private readonly SortedDictionary<string, string> _sortedDictionary;
+
+        public WeChatPayRequest()
+        {
+            _sortedDictionary = new SortedDictionary<string, string>(StringComparer.Ordinal);
+        }
+
+        public virtual void AddParameter(string key, string strValue)
+        {
+            if (string.IsNullOrEmpty(strValue)) return;
+            if (_sortedDictionary.ContainsKey(key)) return;
+
+            _sortedDictionary.Add(key, strValue);
+        }
+
+        public virtual string GetWaitForSignatureStr()
+        {
+            var sb = new StringBuilder();
+            foreach (var kv in _sortedDictionary)
+            {
+                sb.Append(kv.Key).Append('=').Append(kv.Value).Append('&');
+            }
+
+            return sb.ToString().TrimEnd('&');
+        }
+
+        public virtual string ToXmlStr()
+        {
+            var xElement = new XElement("xml", _sortedDictionary.Select(kv => new XElement(kv.Key, kv.Value)));
+
+            using (var memoryStream = new MemoryStream())
+            {
+                xElement.Save(memoryStream);
+                memoryStream.Position = 0;
+                using (StreamReader reader = new StreamReader(memoryStream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
