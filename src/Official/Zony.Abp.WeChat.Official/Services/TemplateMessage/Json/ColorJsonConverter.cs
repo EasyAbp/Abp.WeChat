9aca0e89d6841486e759d44f10418cb7d4565111
GameBelial <243387971@qq.com>
2019-08-31 13:45:38 +0800

    补全微信模板消息发送服务的注释说明信息。

--- src/Official/Zony.Abp.WeChat.Official/Services/TemplateMessage/Json/ColorJsonConverter.cs
+++ src/Official/Zony.Abp.WeChat.Official/Services/TemplateMessage/Json/ColorJsonConverter.cs
@@ -4,6 +4,10 @@ using Newtonsoft.Json;
 
 namespace Zony.Abp.WeChat.Official.Services.TemplateMessage.Json
 {
+    /// <summary>
+    /// 针对于 <see cref="Color"/> 类型定义的 Json 转换器，用于将颜色实例转换为 16 进制字符串。
+    /// 并且实现了从十六进制字符串，转换为 <see cref="Color"/> 实例的功能。
+    /// </summary>
     public class ColorJsonConverter : JsonConverter<Color>
     {
         public override void WriteJson(JsonWriter writer, Color value, JsonSerializer serializer)
