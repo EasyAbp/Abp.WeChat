5cfa479eb7ae3d2ab8dc39c0017711f3840c7961
GameBelial <243387971@qq.com>
2019-10-02 00:41:40 +0800

    增加公众号通用返回值基类。

---
+++ src/Official/Zony.Abp.WeChat.Official/Infrastructure/Models/OfficialCommonResponse.cs
@@ -0,0 +1,13 @@
+using Newtonsoft.Json;
+
+namespace Zony.Abp.WeChat.Official.Infrastructure.Models
+{
+    public class OfficialCommonResponse : IOfficialResponse
+    {
+        [JsonProperty("errmsg")]
+        public string ErrorMessage { get; set; }
+
+        [JsonProperty("errcode")]
+        public int ErrorCode { get; set; }
+    }
+}
