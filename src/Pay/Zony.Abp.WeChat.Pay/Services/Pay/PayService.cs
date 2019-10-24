783c843bb71af5ccf3b93a8aaae7ee2f326a9415
GameBelial <243387971@qq.com>
2019-10-24 13:31:25 +0800

    添加查询订单和关闭订单功能。

--- src/Pay/Zony.Abp.WeChat.Pay/Services/Pay/PayService.cs
+++ src/Pay/Zony.Abp.WeChat.Pay/Services/Pay/PayService.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Xml;
@@ -11,6 +12,8 @@ namespace Zony.Abp.WeChat.Pay.Services.Pay
     {
         protected readonly string UnifiedOrderUrl = "https://api.mch.weixin.qq.com/pay/unifiedorder";
         protected readonly string RefundUrl = "https://api.mch.weixin.qq.com/secapi/pay/refund";
+        protected readonly string QueryOrderUrl = "https://api.mch.weixin.qq.com/pay/orderquery";
+        protected readonly string CloseOrderUrl = "https://api.mch.weixin.qq.com/pay/closeorder";
 
         protected readonly AbpWeChatPayOptions AbpWeChatPayOptions;
 
@@ -22,13 +25,15 @@ namespace Zony.Abp.WeChat.Pay.Services.Pay
             {
                 UnifiedOrderUrl = " https://api.mch.weixin.qq.com/sandboxnew/pay/unifiedorder";
                 RefundUrl = "https://api.mch.weixin.qq.com/sandboxnew/secapi/pay/refund";
+                QueryOrderUrl = "https://api.mch.weixin.qq.com/sandboxnew/pay/orderquery";
+                CloseOrderUrl = "https://api.mch.weixin.qq.com/sandboxnew/pay/closeorder";
             }
         }
 
         /// <summary>
         /// 统一下单功能，支持除付款码支付场景以外的预支付交易单生成。
         /// </summary>
-        public async Task<XmlDocument> UnifiedOrderAsync(string appId, string mchId, string body, string orderNo, int totalFee,
+        public Task<XmlDocument> UnifiedOrderAsync(string appId, string mchId, string body, string orderNo, int totalFee,
             string tradeType,
             string openId = null,
             string attach = null)
@@ -49,21 +54,13 @@ namespace Zony.Abp.WeChat.Pay.Services.Pay
             var signStr = SignatureGenerator.Generate(request, MD5.Create(), AbpWeChatPayOptions.ApiKey);
             request.AddParameter("sign", signStr);
 
-            var result = await WeChatPayApiRequester.RequestAsync(UnifiedOrderUrl, request.ToXmlStr());
-            if (result.SelectSingleNode("/xml/err_code") != null ||
-                result.SelectSingleNode("/xml/return_code")?.InnerText != "SUCCESS" ||
-                result.SelectSingleNode("/xml/return_msg")?.InnerText != "OK")
-            {
-                throw new UserFriendlyException($"调用微信支付接口失败。");
-            }
-
-            return result;
+            return RequestAndGetReturnValueAsync(UnifiedOrderUrl, request);
         }
 
         /// <summary>
         /// 申请退款功能，支持针对指定订单进行退款操作。
         /// </summary>
-        public async Task<XmlDocument> RefundAsync(string appId,string mchId,string orderNo,string refundOrderNo, int orderTotalFee,int refundFee,
+        public Task<XmlDocument> RefundAsync(string appId,string mchId,string orderNo,string refundOrderNo, int orderTotalFee,int refundFee,
             string refundDescription = null)
         {
             var request = new WeChatPayParameters();
@@ -79,8 +76,79 @@ namespace Zony.Abp.WeChat.Pay.Services.Pay
             
             var signStr = SignatureGenerator.Generate(request, MD5.Create(), AbpWeChatPayOptions.ApiKey);
             request.AddParameter("sign", signStr);
+
+            return RequestAndGetReturnValueAsync(RefundUrl, request);
+        }
+
+        #region > 查询订单 <
+        
+        /// <summary>
+        /// 根据微信订单号或者商户订单号，查询订单的详细信息。如果两个参数都被填写，优先使用微信订单号进行查询。
+        /// </summary>
+        /// <param name="appId">微信小程序或公众号的 AppId。</param>
+        /// <param name="mchId">微信支付分配的商户号。</param>
+        /// <param name="weChatOrderNo">微信订单号。</param>
+        /// <param name="orderNo">商户订单号，该订单号是商户系统内部生成的唯一编号。</param>
+        /// <returns>请求的结果，会被转换为 <see cref="XmlDocument"/> 实例并返回。</returns>
+        /// <exception cref="ArgumentException">当微信订单号和商户订单号都为 null 时，会抛出本异常。</exception>
+        public Task<XmlDocument> QueryOrderAsync(string appId, string mchId, string weChatOrderNo = null,string orderNo = null)
+        {
+            if (string.IsNullOrEmpty(weChatOrderNo) && string.IsNullOrEmpty(orderNo))
+            {
+                throw new ArgumentException("微信订单号或商户订单号必须传递一个有效参数。");
+            }
+
+            var request = new WeChatPayParameters();
+            request.AddParameter("appid", appId);
+            request.AddParameter("mch_id", mchId);
+            request.AddParameter("nonce_str", RandomHelper.GetRandom());
+
+            if (string.IsNullOrEmpty(weChatOrderNo))
+            {
+                request.AddParameter("transaction_id",weChatOrderNo);
+            }
+            else
+            {
+                request.AddParameter("out_trade_no",orderNo);
+            }
             
-            var result = await WeChatPayApiRequester.RequestAsync(RefundUrl, request.ToXmlStr());
+            var signStr = SignatureGenerator.Generate(request, MD5.Create(), AbpWeChatPayOptions.ApiKey);
+            request.AddParameter("sign", signStr);
+
+            return RequestAndGetReturnValueAsync(QueryOrderUrl, request);
+        }
+        
+        #endregion
+
+        /// <summary>
+        /// 根据商户订单号，关闭指定的订单。
+        /// </summary>
+        /// <remarks>
+        /// 以下情况需要调用关单接口：商户订单支付失败需要生成新单号重新发起支付，要对原订单号调用关单，避免重复支付；系统下单后，
+        /// 用户支付超时，系统退出不再受理，避免用户继续，请调用关单接口。订单生成后不能马上调用关单接口，最短调用时间间隔为 5 分钟。
+        /// </remarks>
+        /// <param name="appId">微信小程序或公众号的 AppId。</param>
+        /// <param name="mchId">微信支付分配的商户号。</param>
+        /// <param name="orderNo">商户订单号，该订单号是商户系统内部生成的唯一编号。</param>
+        /// <returns>请求的结果，会被转换为 <see cref="XmlDocument"/> 实例并返回。</returns>
+        public Task<XmlDocument> CloseOrderAsync(string appId, string mchId, string orderNo)
+        {
+            var request = new WeChatPayParameters();
+            request.AddParameter("appid", appId);
+            request.AddParameter("mch_id", mchId);
+            request.AddParameter("nonce_str", RandomHelper.GetRandom());
+            
+            request.AddParameter("out_trade_no", orderNo);
+            
+            var signStr = SignatureGenerator.Generate(request, MD5.Create(), AbpWeChatPayOptions.ApiKey);
+            request.AddParameter("sign", signStr);
+
+            return RequestAndGetReturnValueAsync(CloseOrderUrl, request);
+        }
+
+        protected virtual async Task<XmlDocument> RequestAndGetReturnValueAsync(string targetUrl, WeChatPayParameters requestParameters)
+        {
+            var result = await WeChatPayApiRequester.RequestAsync(targetUrl, requestParameters.ToXmlStr());
             if (result.SelectSingleNode("/xml/err_code") != null ||
                 result.SelectSingleNode("/xml/return_code")?.InnerText != "SUCCESS" ||
                 result.SelectSingleNode("/xml/return_msg")?.InnerText != "OK")
--- tests/Zony.Abp.WeChat.Pay.Tests/Services/PayService_Tests.cs
+++ tests/Zony.Abp.WeChat.Pay.Tests/Services/PayService_Tests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading.Tasks;
+using Shouldly;
 using Xunit;
 using Zony.Abp.WeChat.Pay.Services.Pay;
 
@@ -17,12 +18,27 @@ namespace Zony.Abp.WeChat.Pay.Tests.Services
         [Fact]
         public async Task UnifiedOrder_Test()
         {
-            await _payService.UnifiedOrderAsync("wxe32e0204e9db0b1c",
+            var result = await _payService.UnifiedOrderAsync("wxe32e0204e9db0b1c",
                 "1540561391",
                 "测试支付",
                 DateTime.Now.ToString("yyyyMMddHHmmss"),
                 101,
                 TradeType.JsApi);
+            
+            result.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public async Task Ref()
+        {
+            var response = await _payService.RefundAsync("wxe32e0204e9db0b1c",
+                "1540561391",
+                "151515151515",
+                "161616161616",
+                101,
+                50);
+            
+            response.ShouldNotBeNull();
         }
     }
 }
