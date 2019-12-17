f41bd78b198f3cf0614d79a80474bab32c134e2b
GameBelial <243387971@qq.com>
2019-12-17 13:58:19 +0800

    增加隐私字段加密方法。

---
+++ src/Pay/Zony.Abp.WeChat.Pay/Extensions/WeChatPayToolUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Zony.Abp.WeChat.Pay.Extensions
+{
+    public static class WeChatPayToolUtility
+    {
+        public static string Encrypt(this string src, string pemPath)
+        {
+            var x509 = new X509Certificate2(pemPath);
+            var rsa = (RSACryptoServiceProvider) x509.PublicKey.Key;
+            var buffer = rsa.Encrypt(Encoding.UTF8.GetBytes(src), false);
+            return Convert.ToBase64String(buffer);
+        }
+    }
+}
