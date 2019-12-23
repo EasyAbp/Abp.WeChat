d5f8b8a2155bcd6231659a4c88b525f2db3238ac
GameBelial <243387971@qq.com>
2019-12-23 15:26:04 +0800

    完成小微商户进件接口。

--- src/Pay/Zony.Abp.WeChat.Pay/Extensions/WeChatPayToolUtility.cs
+++ src/Pay/Zony.Abp.WeChat.Pay/Extensions/WeChatPayToolUtility.cs
@@ -2,17 +2,38 @@
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Parameters;
 
 namespace Zony.Abp.WeChat.Pay.Extensions
 {
     public static class WeChatPayToolUtility
     {
-        public static string Encrypt(this string src, string pemPath)
+        public static string Encrypt(this string src, byte[] key)
         {
-            var x509 = new X509Certificate2(pemPath);
-            var rsa = (RSACryptoServiceProvider) x509.PublicKey.Key;
-            var buffer = rsa.Encrypt(Encoding.UTF8.GetBytes(src), false);
-            return Convert.ToBase64String(buffer);
+            using var x509 = new X509Certificate2(key);
+            using var rsa = (RSA)x509.PublicKey.Key;
+            var buff = rsa.Encrypt(Encoding.UTF8.GetBytes(src),RSAEncryptionPadding.Pkcs1);
+
+            return Convert.ToBase64String(buff);
+        }
+
+        public static byte[] GetCertificate(string key, string associatedData, string nonce, string cipherText)
+        {
+            var gcmBlockCipher = new GcmBlockCipher(new AesEngine());
+            var aeadParameters = new AeadParameters(
+                new KeyParameter(Encoding.UTF8.GetBytes(key)),
+                128,
+                Encoding.UTF8.GetBytes(nonce),
+                Encoding.UTF8.GetBytes(associatedData));
+            gcmBlockCipher.Init(false, aeadParameters);
+
+            var data = Convert.FromBase64String(cipherText);
+            var plaintext = new byte[gcmBlockCipher.GetOutputSize(data.Length)];
+            var length = gcmBlockCipher.ProcessBytes(data, 0, data.Length, plaintext, 0);
+            gcmBlockCipher.DoFinal(plaintext, length);
+            return plaintext;
         }
     }
 }
