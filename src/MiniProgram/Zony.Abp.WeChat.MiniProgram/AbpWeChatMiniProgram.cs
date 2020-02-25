5d636b7f03582b3e8791917afebc3135da6b67c4
GameBelial <243387971@qq.com>
2020-02-25 21:56:25 +0800

    feat(小程序): 建立小程序模块。

---
+++ src/MiniProgram/Zony.Abp.WeChat.MiniProgram/AbpWeChatMiniProgram.cs
@@ -0,0 +1,10 @@
+﻿using Volo.Abp.Modularity;
+using Zony.Abp.WeChat.Common;
+
+namespace Zony.Abp.WeChat.MiniProgram
+{
+    [DependsOn(typeof(AbpWeChatCommonModule))]
+    public class AbpWeChatMiniProgram : AbpModule
+    {
+    }
+}
