052a3d19576b13849b1f59090e8de31744912c50
gdlcf88 <gdlcf88@outlook.com>
2020-06-30 00:00:52 +0800

    增加微信登录测试

---
+++ tests/EasyAbp.Abp.WeChat.MiniProgram.Tests/AbpWeChatMiniProgramTestBase.cs
@@ -0,0 +1,9 @@
+using EasyAbp.Abp.WeChat.Common.Tests;
+
+namespace EasyAbp.Abp.WeChat.MiniProgram.Tests
+{
+    public class AbpWeChatMiniProgramTestBase : AbpWeChatCommonTestBase<AbpWeChatMiniProgramTestsModule>
+    {
+
+    }
+}
---
+++ tests/EasyAbp.Abp.WeChat.MiniProgram.Tests/AbpWeChatMiniProgramTestsConsts.cs
@@ -0,0 +1,11 @@
+namespace EasyAbp.Abp.WeChat.MiniProgram.Tests
+{
+    public class AbpWeChatMiniProgramTestsConsts
+    {
+        public const string AppId = "wxe32e0204e9db0b1c";
+
+        public const string AppSecret = "19086866683797928dc708f9ec01aa29";
+        
+        public const string JsCode = "";
+    }
+}
