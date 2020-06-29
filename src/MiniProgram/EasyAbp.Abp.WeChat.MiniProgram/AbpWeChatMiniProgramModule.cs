1f3ec7783dc61608a25511cd4d49ee86ecb395fd
gdlcf88 <gdlcf88@outlook.com>
2020-06-29 23:50:18 +0800

    修正模块名称

--- src/MiniProgram/EasyAbp.Abp.WeChat.MiniProgram/AbpWeChatMiniProgramModule.cs
+++ src/MiniProgram/EasyAbp.Abp.WeChat.MiniProgram/AbpWeChatMiniProgramModule.cs
@@ -4,7 +4,7 @@ using EasyAbp.Abp.WeChat.Common;
 namespace EasyAbp.Abp.WeChat.MiniProgram
 {
     [DependsOn(typeof(AbpWeChatCommonModule))]
-    public class AbpWeChatMiniProgram : AbpModule
+    public class AbpWeChatMiniProgramModule : AbpModule
     {
     }
 }
