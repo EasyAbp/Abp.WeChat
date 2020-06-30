58d9e624239a48675816aa14ea98664a9042383a
gdlcf88 <gdlcf88@outlook.com>
2020-06-30 18:44:02 +0800

    替换AesHelper.AesDecrypt()实现

--- src/MiniProgram/EasyAbp.Abp.WeChat.MiniProgram/Infrastructure/AesHelper.cs
+++ src/MiniProgram/EasyAbp.Abp.WeChat.MiniProgram/Infrastructure/AesHelper.cs
@@ -1,54 +1,28 @@
 ﻿using System;
-using System.IO;
 using System.Security.Cryptography;
 using System.Text;
-using Volo.Abp;
 
 namespace EasyAbp.Abp.WeChat.MiniProgram.Infrastructure
 {
     public static class AesHelper
     {
-        public static byte[] AesEncrypt(byte[] data, byte[] iv, string strKey)
+        public static string AesDecrypt(string inputData, string iv, string key)
         {
-            SymmetricAlgorithm des = Aes.Create();
-
-            var byteArray = data;
+            var encryptedData = Convert.FromBase64String(inputData.Replace(" ", "+"));
             
-            des.Key = Encoding.UTF8.GetBytes(strKey.PadRight(32));
-            des.IV = iv;
-
-            using var ms = new MemoryStream();
-            using var cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
-            
-            cs.Write(byteArray, 0, byteArray.Length);
-            cs.FlushFinalBlock();
-            var cipherBytes = ms.ToArray();   
-            
-            return cipherBytes;
-        }
-        
-        public static byte[] AesDecrypt(byte[] data, byte[] iv, string strKey)
-        {
-            SymmetricAlgorithm des = Aes.Create();
-
-            des.Key = Encoding.UTF8.GetBytes(strKey.PadRight(32));
-            des.IV = iv;
-
-            using var ms = new MemoryStream(data);
-            using var cs = new CryptoStream(ms, des.CreateDecryptor(), CryptoStreamMode.Read);
-            using var originalMemory = new MemoryStream();
-            
-            var buffer = new byte[1024];
-            int readBytes;
-            
-            while ((readBytes = cs.Read(buffer, 0, buffer.Length)) > 0)
+            var rijndaelCipher = new RijndaelManaged
             {
-                originalMemory.Write(buffer, 0, readBytes);
-            }
-
-            var decryptBytes = originalMemory.ToArray();
-
-            return decryptBytes;
+                Key = Convert.FromBase64String(key.Replace(" ", "+")),
+                IV = Convert.FromBase64String(iv.Replace(" ", "+")),
+                Mode = CipherMode.CBC,
+                Padding = PaddingMode.PKCS7
+            };
+            
+            var transform = rijndaelCipher.CreateDecryptor();
+            
+            var plainText = transform.TransformFinalBlock(encryptedData, 0, encryptedData.Length);
+            
+            return Encoding.UTF8.GetString(plainText);
         }
     }
 }
