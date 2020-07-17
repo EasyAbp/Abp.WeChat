f6623d1a5868cfe1e0aec9ceb32cefc071b483f8
gdlcf88 <gdlcf88@outlook.com>
2020-07-17 22:11:26 +0800

    补充额外的Code2SessionAsync方法重载

--- src/MiniProgram/EasyAbp.Abp.WeChat.MiniProgram/Services/Login/LoginService.cs
+++ src/MiniProgram/EasyAbp.Abp.WeChat.MiniProgram/Services/Login/LoginService.cs
@@ -1,5 +1,6 @@
 using System.Net.Http;
 using System.Threading.Tasks;
+using EasyAbp.Abp.WeChat.MiniProgram.Infrastructure.OptionsResolve;
 
 namespace EasyAbp.Abp.WeChat.MiniProgram.Services.Login
 {
@@ -8,6 +9,25 @@ namespace EasyAbp.Abp.WeChat.MiniProgram.Services.Login
     /// </summary>
     public class LoginService : CommonService
     {
+        private readonly IWeChatMiniProgramOptionsResolver _optionsResolver;
+
+        public LoginService(IWeChatMiniProgramOptionsResolver optionsResolver)
+        {
+            _optionsResolver = optionsResolver;
+        }
+        
+        /// <summary>
+        /// 登录凭证校验。通过 wx.login 接口获得临时登录凭证 code 后传到开发者服务器调用此接口完成登录流程。
+        /// </summary>
+        /// <param name="jsCode">登录时获取的 code</param>
+        /// <param name="grantType">授权类型，此处只需填写 authorization_code</param>
+        public Task<Code2SessionResponse> Code2SessionAsync(string jsCode, string grantType = "authorization_code")
+        {
+            var options = _optionsResolver.Resolve();
+
+            return Code2SessionAsync(options.AppId, options.AppSecret, jsCode, grantType);
+        }
+        
         /// <summary>
         /// 登录凭证校验。通过 wx.login 接口获得临时登录凭证 code 后传到开发者服务器调用此接口完成登录流程。
         /// </summary>
