1023e7cbbd0489aa0da4fed25cc68eeb4c5319d1
seventh88 <5856868x@gmail.com>
2020-12-03 13:57:45 +0800

    add WeChatOfficialAsyncLocal

--- src/Official/EasyAbp.Abp.WeChat.Official/AbpWeChatOfficialModule.cs
+++ src/Official/EasyAbp.Abp.WeChat.Official/AbpWeChatOfficialModule.cs
@@ -1,11 +1,22 @@
 ﻿using Volo.Abp.Modularity;
 using EasyAbp.Abp.WeChat.Common;
+using EasyAbp.Abp.WeChat.Official.Infrastructure.OptionsResolve;
+using EasyAbp.Abp.WeChat.Official.Infrastructure.OptionsResolve.Contributors;
 
 namespace EasyAbp.Abp.WeChat.Official
 {
     [DependsOn(typeof(AbpWeChatCommonModule))]
     public class AbpWeChatOfficialModule : AbpModule 
     {
-        
+        public override void PostConfigureServices(ServiceConfigurationContext context)
+        {
+            Configure<AbpWeChatOfficialResolveOptions>(options =>
+            {
+                if (!options.WeChatOfficialOptionsResolveContributors.Exists(x => x.Name == AsyncLocalOptionsResolveContributor.ContributorName))
+                {
+                    options.WeChatOfficialOptionsResolveContributors.Insert(0, new AsyncLocalOptionsResolveContributor());
+                }
+            });
+        }
     }
 }
