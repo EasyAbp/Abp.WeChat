1d32253f6cf500fdf5ad3ff4e3e37b592f82000d
Slarkerino Company Desktop <307221214@qq.com>
2021-04-16 16:07:06 +0800

    Add Wechat Official Login Service

---
+++ src/Official/EasyAbp.Abp.WeChat.Official/Services/Login/Code2AccessTokenResponse.cs
@@ -0,0 +1,29 @@
+using EasyAbp.Abp.WeChat.Official.Infrastructure.Models;
+using Newtonsoft.Json;
+
+namespace EasyAbp.Abp.WeChat.Official.Services.Login
+{
+    public class Code2AccessTokenResponse : IOfficialResponse
+    {
+        [JsonProperty("errmsg")]
+        public string ErrorMessage { get; set; }
+
+        [JsonProperty("errcode")]
+        public int ErrorCode { get; set; }
+
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("openid")]
+        public string OpenId { get; set; }
+
+        [JsonProperty("refresh_token")]
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ src/Official/EasyAbp.Abp.WeChat.Official/Services/Login/LoginService.cs
@@ -0,0 +1,47 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using EasyAbp.Abp.WeChat.Official.Infrastructure.OptionsResolve;
+
+namespace EasyAbp.Abp.WeChat.Official.Services.Login
+{
+    /// <summary>
+    /// 公众号登录服务。
+    /// </summary>
+    public class LoginService : CommonService
+    {
+        private readonly IWeChatOfficialOptionsResolver _optionsResolver;
+
+        public LoginService(IWeChatOfficialOptionsResolver optionsResolver)
+        {
+            _optionsResolver = optionsResolver;
+        }
+        
+        /// <summary>
+        /// 登录凭证校验。通过 wx.login 接口获得临时登录凭证 code 后传到开发者服务器调用此接口完成登录流程。
+        /// </summary>
+        /// <param name="code">登录时获取的 code</param>
+        /// <param name="grantType">授权类型，此处只需填写 authorization_code</param>
+        public async Task<Code2AccessTokenResponse> Code2SessionAsync(string code, string grantType = "authorization_code")
+        {
+            var options = await _optionsResolver.ResolveAsync();
+
+            return await Code2SessionAsync(options.AppId, options.AppSecret, code, grantType);
+        }
+        
+        /// <summary>
+        /// 登录凭证校验。通过 wx.login 接口获得临时登录凭证 code 后传到开发者服务器调用此接口完成登录流程。
+        /// </summary>
+        /// <param name="appId">公众号 appId</param>
+        /// <param name="appSecret">公众号 appSecret</param>
+        /// <param name="code">登录时获取的 code</param>
+        /// <param name="grantType">授权类型，此处只需填写 authorization_code</param>
+        public Task<Code2AccessTokenResponse> Code2SessionAsync(string appId, string appSecret, string code, string grantType = "authorization_code")
+        {
+            const string targetUrl = "https://api.weixin.qq.com/sns/oauth2/access_token?";
+
+            var request = new Code2AccessTokenRequest(appId, appSecret, code, grantType);
+
+            return WeChatOfficialApiRequester.RequestAsync<Code2AccessTokenResponse>(targetUrl, HttpMethod.Get, request, false);
+        }
+    }
+}
