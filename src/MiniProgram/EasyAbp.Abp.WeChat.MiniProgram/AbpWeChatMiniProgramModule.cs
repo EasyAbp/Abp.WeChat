61d7dbb824cf5566cc8ee8a59c28d28136575587
gdlcf88 <gdlcf88@outlook.com>
2022-12-17 14:03:08 +0800

    Introduce `EasyAbp.Abp.WeChat.MiniProgram.Abstractions`

--- src/MiniProgram/EasyAbp.Abp.WeChat.MiniProgram/AbpWeChatMiniProgramModule.cs
+++ src/MiniProgram/EasyAbp.Abp.WeChat.MiniProgram/AbpWeChatMiniProgramModule.cs
@@ -5,7 +5,10 @@ using EasyAbp.Abp.WeChat.MiniProgram.Infrastructure.OptionsResolve.Contributors;
 
 namespace EasyAbp.Abp.WeChat.MiniProgram
 {
-    [DependsOn(typeof(AbpWeChatCommonModule))]
+    [DependsOn(
+        typeof(AbpWeChatCommonModule),
+        typeof(AbpWeChatMiniProgramAbstractionsModule)
+    )]
     public class AbpWeChatMiniProgramModule : AbpModule
     {
         public override void PostConfigureServices(ServiceConfigurationContext context)
