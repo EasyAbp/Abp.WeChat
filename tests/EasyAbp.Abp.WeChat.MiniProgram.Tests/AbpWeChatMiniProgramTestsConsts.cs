6d6afbc92f8b7a5d49ec42645b8dde9235bacb93
real-zony <zony@myzony.com>
2024-01-07 18:43:45 +0800

    refactor: Removed the test account's keys.

--- tests/EasyAbp.Abp.WeChat.MiniProgram.Tests/AbpWeChatMiniProgramTestsConsts.cs
+++ tests/EasyAbp.Abp.WeChat.MiniProgram.Tests/AbpWeChatMiniProgramTestsConsts.cs
@@ -2,9 +2,9 @@ namespace EasyAbp.Abp.WeChat.MiniProgram.Tests
 {
     public class AbpWeChatMiniProgramTestsConsts
     {
-        public const string AppId = "wxe32e0204e9db0b1c";
+        public const string AppId = "";
 
-        public const string AppSecret = "19086866683797928dc708f9ec01aa29";
+        public const string AppSecret = "";
         
         public const string JsCode = "";
     }
--- tests/EasyAbp.Abp.WeChat.Official.Tests/AbpWeChatOfficialTestsConsts.cs
+++ tests/EasyAbp.Abp.WeChat.Official.Tests/AbpWeChatOfficialTestsConsts.cs
@@ -2,8 +2,8 @@ namespace EasyAbp.Abp.WeChat.Official.Tests
 {
     public class AbpWeChatOfficialTestsConsts
     {
-        public const string AppId = "wxe32e0204e9db0b1c";
+        public const string AppId = "";
 
-        public const string AppSecret = "19086866683797928dc708f9ec01aa29";
+        public const string AppSecret = "";
     }
 }
