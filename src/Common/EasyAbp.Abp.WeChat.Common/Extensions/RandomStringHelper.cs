56270cb7f50bab2624d6bf99a820e1eeb250616a
real-zony <zony@myzony.com>
2024-01-08 10:57:21 +0800

    test: Adjusted the test cases.

--- src/Common/EasyAbp.Abp.WeChat.Common/Extensions/RandomStringHelper.cs
+++ src/Common/EasyAbp.Abp.WeChat.Common/Extensions/RandomStringHelper.cs
@@ -5,13 +5,16 @@ namespace EasyAbp.Abp.WeChat.Common.Extensions
 {
     public static class RandomStringHelper
     {
-        public static string GetRandomString()
+        public static string GetRandomString(int length = 30)
         {
-            char[] constant = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+            char[] constant =
+            [
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+            ];
 
             var sb = new StringBuilder();
             var rd = new Random();
-            for (int i = 0; i < 30; i++)
+            for (var i = 0; i < length; i++)
             {
                 sb.Append(constant[rd.Next(36)]);
             }
--- tests/EasyAbp.Abp.WeChat.Pay.Tests/AbpWeChatPayTestConsts.cs
+++ tests/EasyAbp.Abp.WeChat.Pay.Tests/AbpWeChatPayTestConsts.cs
@@ -5,7 +5,9 @@
         public const bool IsSandBox = false;
         public const string MchId = "";
         public const string ApiKey = "";
+        public const string ApiV3Key = "";
         public const string AppId = "";
         public const string OpenId = "";
+        public const string NotifyUrl = "";
     }
 }
