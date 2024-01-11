a6e0a709e81d440cbcdb287ab64c2edb615777cf
real-zony <zony@myzony.com>
2024-01-11 11:57:12 +0800

    feat: Added H5 and Native payment options.

--- src/Pay/EasyAbp.Abp.WeChat.Pay/Services/BasicPayment/H5Payment/H5PaymentService.cs
+++ src/Pay/EasyAbp.Abp.WeChat.Pay/Services/BasicPayment/H5Payment/H5PaymentService.cs
@@ -1,6 +1,23 @@
-﻿namespace EasyAbp.Abp.WeChat.Pay.Services.BasicPayment.H5Payment;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using EasyAbp.Abp.WeChat.Pay.Options;
+using EasyAbp.Abp.WeChat.Pay.Services.BasicPayment.Models;
+using Volo.Abp.DependencyInjection;
 
-public class H5PaymentService
+namespace EasyAbp.Abp.WeChat.Pay.Services.BasicPayment.H5Payment;
+
+public class H5PaymentService : BasicPaymentService
 {
-    
+    public const string CreateOrderUrl = "https://api.mch.weixin.qq.com/v3/pay/transactions/h5";
+
+    public H5PaymentService(AbpWeChatPayOptions options,
+        IAbpLazyServiceProvider lazyServiceProvider) : base(options,
+        lazyServiceProvider)
+    {
+    }
+
+    public Task<CreateOrderResponse> CreateOrderAsync(CreateOrderRequest request)
+    {
+        return ApiRequester.RequestAsync<CreateOrderResponse>(HttpMethod.Post, CreateOrderUrl, request);
+    }
 }
--- src/Pay/EasyAbp.Abp.WeChat.Pay/Services/BasicPayment/NativePayment/NativePaymentService.cs
+++ src/Pay/EasyAbp.Abp.WeChat.Pay/Services/BasicPayment/NativePayment/NativePaymentService.cs
@@ -1,6 +1,23 @@
-﻿namespace EasyAbp.Abp.WeChat.Pay.Services.BasicPayment.NativePayment;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using EasyAbp.Abp.WeChat.Pay.Options;
+using EasyAbp.Abp.WeChat.Pay.Services.BasicPayment.Models;
+using Volo.Abp.DependencyInjection;
 
-public class NativePaymentService
+namespace EasyAbp.Abp.WeChat.Pay.Services.BasicPayment.NativePayment;
+
+public class NativePaymentService : BasicPaymentService
 {
-    
+    public const string CreateOrderUrl = "https://api.mch.weixin.qq.com/v3/pay/transactions/native";
+
+    public NativePaymentService(AbpWeChatPayOptions options,
+        IAbpLazyServiceProvider lazyServiceProvider) : base(options,
+        lazyServiceProvider)
+    {
+    }
+
+    public Task<CreateOrderResponse> CreateOrderAsync(CreateOrderRequest request)
+    {
+        return ApiRequester.RequestAsync<CreateOrderResponse>(HttpMethod.Post, CreateOrderUrl, request);
+    }
 }
