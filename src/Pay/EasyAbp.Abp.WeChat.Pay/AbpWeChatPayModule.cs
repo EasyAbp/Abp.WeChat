715a3f330a285315e501a7ca894a9b106d40e6b4
real-zony <zony@myzony.com>
2024-01-15 21:19:01 +0800

    refactor: Removed the unused NewtonJson module.

--- src/Pay/EasyAbp.Abp.WeChat.Pay/AbpWeChatPayModule.cs
+++ src/Pay/EasyAbp.Abp.WeChat.Pay/AbpWeChatPayModule.cs
@@ -1,7 +1,6 @@
 ﻿using EasyAbp.Abp.WeChat.Common;
 using Microsoft.Extensions.DependencyInjection;
 using Volo.Abp.BlobStoring;
-using Volo.Abp.Json.Newtonsoft;
 using Volo.Abp.Modularity;
 
 namespace EasyAbp.Abp.WeChat.Pay
@@ -9,8 +8,7 @@ namespace EasyAbp.Abp.WeChat.Pay
     [DependsOn(
         typeof(AbpWeChatCommonModule),
         typeof(AbpBlobStoringModule),
-        typeof(AbpWeChatPayAbstractionsModule),
-        typeof(AbpJsonNewtonsoftModule)
+        typeof(AbpWeChatPayAbstractionsModule)
     )]
     public class AbpWeChatPayModule : AbpModule
     {
